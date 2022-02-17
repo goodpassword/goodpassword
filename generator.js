179fd2909959be2addb28212edf22761bd126e31
Kenneth <kennethpete@gmail.com>
2022-02-17 03:18:56 +0100

    Probability weights added to ranges + some structure improvements

--- generator.js
+++ generator.js
@@ -1,36 +1,51 @@
+class Range {
+	constructor(low, high, name, weight) {
+		this.low = low;
+		this.high = high;
+		this.name = name;
+		this.weight = weight;
+	}
+
+	pick(i) {
+		i = i % (this.high - this.low + 1);
+		return this.low === 0 ? specialChars[i] : String.fromCharCode(this.low + i);
+	}
+}
+
 const ranges = [
-	[0, 17, "selectSpecial"],
-	['a'.charCodeAt(0), 'z'.charCodeAt(0), "selectLc"],
-	['A'.charCodeAt(0), 'Z'.charCodeAt(0), "selectUc"],
-	[0x30, 0x39, "selectNumbers"]
+	new Range(0, 17, "selectSpecial", 1),
+	new Range('a'.charCodeAt(0), 'z'.charCodeAt(0), "selectLc", 5),
+	new Range('A'.charCodeAt(0), 'Z'.charCodeAt(0), "selectUc", 5),
+	new Range(0x30, 0x39, "selectNumbers", 3)
 ]
 
 var selected;
-var totalLength = 0;
+var totalWeight = 0;
 
 const specialChars = "!@#$%^&*()_-+=/?><";
 
-function charOf(i) {
-	i = i % totalLength;
+function charOf(i, j) {
+	j = j % totalWeight;
 	let r = 0;
 
-	while(selected[r][0] + i > selected[r][1]) {
-		i -= selected[r][1] - selected[r][0] + 1;
+	while(selected[r].weight <= j) {
+		j -= selected[r].weight;
 		r++;
 	}
 
-	return selected[r][0] === 0 ? specialChars[i] : String.fromCharCode(selected[r][0] + i);
+	return selected[r].pick(i);
 }
 
 function generate(len) {
-	selected = ranges.filter(x => document.getElementById(x[2]).checked);
+	selected = ranges.filter(x => document.getElementById(x.name).checked);
 	if(selected.length === 0) {
 		return "";
 	}
-	totalLength = selected.reduce((p, c) => p + c[1] - c[0] + 1, 0);
-	let binary = new Uint8Array(len);
+	totalWeight = selected.reduce((p, c) => p + c.weight, 0);
+
+	let binary = new Uint16Array(len);
 	crypto.getRandomValues(binary);
-	return Array.from(binary).map(x => charOf(x)).join('');
+	return Array.from(binary).map(x => charOf(x >> 8, x % 256)).join('');
 }
 
 function output() {
